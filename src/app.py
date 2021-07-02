607b3ca30eecb8c439d63520b474eac116d8e1e4
Icesotope <53615543+AbsoluteZeroist@users.noreply.github.com>
2021-07-03 01:42:43 +0800

    Create app.py

---
+++ src/app.py
@@ -0,0 +1,22 @@
+import pygame
+
+
+def main():
+
+    pygame.init()
+
+    pygame.display.set_caption("Hello Game")
+
+    screen = pygame.display.set_mode((720,480))
+
+    running = True
+
+    #main loop
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+
+if __name__ == "__main__":
+    main()
