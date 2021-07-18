e6b0d8ca5f1e8fa621b811b7e6dcdbafd5d3c325
Icesotope <53615543+AbsoluteZeroist@users.noreply.github.com>
2021-07-18 12:44:45 +0800

    made divs and added conditions for game modes.

--- src/app.py
+++ src/app.py
@@ -9,10 +9,10 @@ def text_objects(text, font):
     return textSurface, textSurface.get_rect()
 
 
-def message_display(text):
+def message_display(text, xPos, yPos):
     textFont = pygame.font.Font('freesansbold.ttf',115)
     TextSurf, TextRect = text_objects(text, textFont)
-    TextRect.center = ((screenWidth/2),(screenHeight/2))
+    TextRect.center = ((xPos),(yPos))
     screen.blit(TextSurf, TextRect)
 
 
@@ -25,77 +25,108 @@ def checkCollide(gameEvents, rightXCoord, topYCoord):
     for event in gameEvents:
         if event.type == pygame.MOUSEBUTTONDOWN:
             pos = pygame.mouse.get_pos()
-            print(pos)
 
-            print(pos[0] > rightXCoord and pos[0] < leftXCoord and pos[1] > topYCoord and pos[1] < botYCoord)
-            
+
+def checkFollowing():
+    pass    
+
+
+def createMenu():
+    pass
+    
 
 def main():
     pygame.init()
     pygame.display.set_caption("Hello Game")
-
-    xChange = 30
-    yChange = 20
-    x = screenWidth * 0.45 #centered(in small window...)
-    y = screenHeight * 0.8 #bottom page
-    #^ these two var may need to change in the future. prob make local in functions. 
     
     display = pygame.display
     global screen 
     screen = display.set_mode((screenWidth,screenHeight), pygame.RESIZABLE)
     display.init()
 
+    xChange = 30
+    yChange = 20
+    x = screenWidth * 0.45 #centered(in small window...)
+    y = screenHeight * 0.8 #bottom page
+    #^ these two var may need to change in the future. prob make local in functions. 
+
     def pause(t):
         pygame.time.delay(t)
 
-    #main loop
-    gameOver = False
+    #loop for catch the box
     running = True
+    gameStarted = False
+    gameOver = False
+    gameMode = 0
+    
 
     while running:
+        #game init stuff
+        #------------------------#
         gameEvents = pygame.event.get()
 
         for event in gameEvents:
             if event.type == pygame.QUIT:
                 running = False
                 pygame.display.quit()
+        #------------------------#
 
-        mvingSq = RectBlit(colours['green'], (x, y, 100, 100))
-        mvingSq.createBlit(screen)
+
+        #menu 
+        #------------------------#
 
         #------------------------#
-        if x < 10:
-            y -= yChange
-            if y < 10:
+
+        if gameStarted:
+            #square moving
+            #------------------------#
+            mvingSq = RectBlit(colours['green'], (x, y, 100, 100))
+            mvingSq.createBlit(screen)
+
+
+            if x < 10:
+                y -= yChange
+                if y < 10:
+                    x += xChange
+            elif y < 10:
                 x += xChange
-        elif y < 10:
-            x += xChange
-            if x > screenWidth - 120:
+                if x > screenWidth - 120:
+                    y += yChange
+            elif x > screenWidth - 120:
                 y += yChange
-        elif x > screenWidth - 120:
-            y += yChange
-            if y > screenHeight - 110:
+                if y > screenHeight - 110:
+                    x -= xChange
+            elif y > screenHeight - 110:
                 x -= xChange
-        elif y > screenHeight - 110:
-            x -= xChange
-        #------------------------#
+            #------------------------#
 
-        checkCollide(gameEvents, x, y)
-        if pos[0] > x and pos[0] < x + 100 and pos[1] > y and pos[1] < y + 100:
-            xChange = 0
-            yChange = 0
-            message_display('Stopped!')
-            gameOver = True
-        
-        pygame.display.update()
-        pause(50)
+            #click the box game
+            #------------------------#
+            if gameMode == 1:
+                checkCollide(gameEvents, x, y)
+                if pos[0] > x and pos[0] < x + 100 and pos[1] > y and pos[1] < y + 100:
+                    xChange = 0
+                    yChange = 0
+                    message_display('Stopped!', screenWidth/2, screenHeight/2)
+                    gameOver = True
+            #------------------------#
 
-        if not gameOver == True:
-            screen.fill(colours["black"])
-            print("gameover = {}".format(not gameOver == True))
+            #help the box game
+            #------------------------#
+            if gameMode == 2:
+                pass
+            #------------------------#
+            
+            #refesh display
+            #------------------------#
+            pygame.display.update()
+            pause(50)
 
-        #print("({},{})".format(x,y))
-        pygame.display.update()
+            if gameOver:
+                screen.fill(colours["black"])
+
+            pygame.display.update()
+            #------------------------#
         
 
 if __name__ == "__main__":
